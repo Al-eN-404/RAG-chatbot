6beff61c04e58d7b36df2f75c662728af06aab2d
Alen James <132215537+Al-eN-404@users.noreply.github.com>
2026-06-10 08:13:09 +0530

    Rename chunk_page to chunk_pages and clean up
    Updated function name for clarity and removed unused code.
    removed unsed parts,
    avoided saving chunks in a json file

--- chunker.py
+++ chunker.py
@@ -1,9 +1,7 @@
-import json
-
 from scraper import crawl_website
 from langchain_text_splitters import (RecursiveCharacterTextSplitter)
 
-def chunk_page(pages):
+def chunk_pages(pages):
     splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
     
     chunks=[]
@@ -15,26 +13,3 @@ def chunk_page(pages):
             chunks.append({"url":page["url"], "text":chunk})
     
     return chunks
-
-pages=crawl_website("https://en.wikipedia.org/wiki/Transport", maxpages=3)
-
-chunks = chunk_page(pages)
-
-print(f"Total Pages: {len(pages)}")
-print(f"Total Chunks: {len(chunks)}")
-print("\nFirst Chunk:\n")
-print(chunks[0]["text"][:500])
-
-# ### Save Chunks to JSON File
-with open(
-    "chunks.json",
-    "w",
-    encoding="utf-8"
-) as f:
-
-    json.dump(
-        chunks,
-        f,
-        ensure_ascii=False,
-        indent=2
-    )
